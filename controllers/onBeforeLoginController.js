a83757e1dc67edc7b5b20b543998f18bebd98c35
Tanishq Jain <jaintanishq1112@gmail.com>
2022-08-29 17:40:16 +0530

    Added the code for Login

--- controllers/onBeforeLoginController.js
+++ controllers/onBeforeLoginController.js
@@ -1,4 +1,13 @@
 const security = require("../common/security.js");
+const { TableClient, AzureNamedKeyCredential } = require("@azure/data-tables");
+
+// Enter your storage account name and shared key
+const account = process.env['STORAGE_ACCOUNT_NAME'];
+const accountKey = process.env['STORAGE_ACCOUNT_KEY'];;
+const tableName = process.env['STORAGE_ACCOUNT_TABLE_NAME'];
+
+const credential = new AzureNamedKeyCredential(account, accountKey);
+const tableClient = new TableClient(`https://${account}.table.core.windows.net`, `${tableName}`, credential);
 
 module.exports = {
 
@@ -8,13 +17,22 @@ module.exports = {
 
             const jws = ctx.request.query.jws || (ctx.request.body && ctx.request.body.jws);
             console.log('onBeforeLogin JWS: ' + jws);
-            let token = await security.verifyClientAssertionAndFetchPayload(jws);
-            console.log('onBeforeLogin API Key: ' + token.apiKey);
-            console.log('onBeforeLogin data' + token.data)
 
-            response = {
-                "status": "OK"
-            };
+            let token = await security.verifyClientAssertionAndFetchPayload(jws);
+            
+            let response = {
+                "status": "FAIL",
+                "data": {
+                    "userFacingErrorMessage": "You are not authorized"
+                }
+            }
+
+            if(await isUserApproved(token.data.params.loginId)){
+                response = {
+                    "status": "OK"
+                }
+            }
+            
             ctx.body = response;
             
         }
@@ -25,3 +43,43 @@ module.exports = {
     }
 
 };
+
+async function isUserApproved(userName){
+    try {
+        let result = await tableClient.getEntity('ApprovalList',userName);
+        if (result.isApproved === true){
+            return true
+        }
+        else{
+            return false
+    }
+    } catch (error) {
+        return false
+    }
+    
+}
+
+async function createUserRecord(userName){
+    
+    try {
+        const record = {
+            partitionKey: "ApprovalList",
+            rowKey: userName,
+            isApproved: false
+          };
+        
+        let result = await tableClient.createEntity(record);
+
+        if(result.etag !== '' && result.etag !== null){
+            return true
+        }
+        else{
+            console.log('Error in inserting record in table: ' + result)
+            return false
+        }
+
+    } catch (error) {
+        console.log('Error in inserting record in table: ' + error)
+    }
+
+}
